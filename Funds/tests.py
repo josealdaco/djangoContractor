214f6fc608bfba93dd019fe38f0cd7097093e2ba
siko408 <gamerbloxhd@gmail.com>
2019-12-13 12:19:34 -0800

    Added 4 tests overall, 2 for Funds and pagesAPI

--- Funds/tests.py
+++ Funds/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+import unittest
 # Create your tests here.
+
+
+class FundPageLoad(TestCase):
+    """ Test if the page is online"""
+    def test_fund_load(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_charityPage(self):
+        """ Test if page is being redirected"""
+        response = self.client.get('/charityPage')
+        self.assertEqual(response.status_code, 301)
--- pagesAPI/tests.py
+++ pagesAPI/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import unittest
+from django.test import TestCase, Client
+import requests
 
 # Create your tests here.
+
+
+class PageAPIViewer(TestCase):
+    """ Check if API is getting data"""
+    def test_pageAPI(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_API(self):
+        """ Test that API is working properly, no 404"""
+        response = requests.get('https://charitywebsite.herokuapp.com/api/pages/?format=json')
+        self.assertEqual(response.status_code, 200)
