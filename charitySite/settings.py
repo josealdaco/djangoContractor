49f09b4fcda6d81bd49c7f25d3297213f53277d8
siko408 <gamerbloxhd@gmail.com>
2019-12-13 02:15:41 -0800

    hiding api key

--- charitySite/settings.py
+++ charitySite/settings.py
@@ -20,9 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['5dwyz(d**o(#22-wv+4u35k6ge5kt7&ysmwg-jiuz()s-uozkj']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'charitywebsite.herokuapp.com']
 
